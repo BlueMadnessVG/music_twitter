dd2517d65a154ac1b0d3569b3596f912b6ceea85
axelito-sl <sanchez.axel3amp@gmail.com>
2022-11-24 21:25:46 -0700

    24-11-22
    Ya quedó gestión de categorías y editar perfil, ya con algunos estilos corregidos pero ya todo jala

--- music_twitter/src/app/modal-addcategoria/modal-addcategoria.component.ts
+++ music_twitter/src/app/modal-addcategoria/modal-addcategoria.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AdminService } from '../servicios/admin.service';
 @Component({
   selector: 'app-modal-addcategoria',
   templateUrl: './modal-addcategoria.component.html',
   styleUrls: ['./modal-addcategoria.component.css']
 })
 export class ModalAddcategoriaComponent implements OnInit {
-  flag=false;
-  constructor() { }
+  frmaddcat!:FormGroup
+  constructor(private fb:FormBuilder,private Admservice:AdminService) { }
 
-  showmodal(){
-    this.flag=true;
+  agregarcat(){
+    alert(this.frmaddcat.controls['nombre'].value);
   }
 
-
   ngOnInit(): void {
+    this.initform();
+  }
+
+  initform(){
+    this.frmaddcat=this.fb.group({
+      nombre:['',Validators,require],
+  });
   }
 
 }
